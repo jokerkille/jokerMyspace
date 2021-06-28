2476b9e5d4cbe006ebb9af414d3f89ae5837d3a8
jokerlu <670115413@qq.com>
2021-06-28 23:21:09 +0800

    firtst commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,11 @@
+/**
+ * @author: joker lu
+ * @Date: 2021/6/28 23:14
+ * @Version: 1.0
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
